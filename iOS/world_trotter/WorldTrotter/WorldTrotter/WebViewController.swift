dbda1e981effed9bb268aa8ca7eca8a9bfa9818e
arturo <arturo@interpegasus.com>
2016-08-19 20:42:16 -0500

    Book Ch 6

---
+++ iOS/world_trotter/WorldTrotter/WorldTrotter/WebViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by Arturo Gomez on 7/25/16.
+//  Copyright © 2016 Arturo Gomez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var webPage:UIWebView!
+    
+    
+}
