479212c6f1853703be3f84c47a6341d1a26ec198
arturo <arturo@interpegasus.com>
2016-08-20 18:08:50 -0500

    iOS Ch Bronze

--- iOS/world_trotter/WorldTrotter/WorldTrotter/WebViewController.swift
+++ iOS/world_trotter/WorldTrotter/WorldTrotter/WebViewController.swift
@@ -7,9 +7,30 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-    @IBOutlet var webPage:UIWebView!
+import WebKit
+
+class WebViewController: UIViewController {
+    
+    var webView: WKWebView!
+    
+    @IBOutlet var myNewWebView: UIWebView!
+    
+    override func loadView() {
+        
+        webView = WKWebView()
+        view = webView
+        let url = NSURL(string: "https://facebook.com")!
+        webView.loadRequest(NSURLRequest(URL: url))
+    }
     
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("WebViewController loaded its view")
+        
+    }
+
+
 }
