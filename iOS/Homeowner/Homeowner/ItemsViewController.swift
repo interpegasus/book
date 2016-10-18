9a21d7e98e76627e1b655773e4dd2ca164d4662d
Arturo G <arturo@interpegasus.com>
2016-10-17 19:03:13 -0500

    iOS

---
+++ iOS/Homeowner/Homeowner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homeowner
+//
+//  Created by Arturo Gomez on 9/23/16.
+//  Copyright © 2016 Arturo Gomez. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+
+}
---
+++ iOS/Quiz 2/Quiz 2/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  Quiz 2
+//
+//  Created by Arturo Gomez on 10/14/16.
+//  Copyright © 2016 Arturo Gomez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = ["Fropm what is cognac made?",
+                               "What is 7+7",
+                               "What is the capital of Vermont"]
+    
+    let answers: [String] = ["Grapes",
+                             "14",
+                             "Montpellier"]
+    
+    var currenQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: AnyObject){
+        currenQuestionIndex += 1
+        if currenQuestionIndex == questions.count{
+            currenQuestionIndex = 0
+        }
+        let question: String = questions[currenQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "?"
+        animateLabelTransition()
+    }
+    
+    @IBAction func showAnswer(_ sender: AnyObject){
+        let answer: String = answers[currenQuestionIndex]
+        answerLabel.text = answer
+    }
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currenQuestionIndex]
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func animateLabelTransition() {
+        let animationClosure = { () -> Void in
+            self.questionLabel.alpha = 1
+            self.answerLabel.alpha = 1
+            let myColor = UIColor.init(red: 1, green: 0.7, blue: 0.8, alpha: 0.9)
+            self.questionLabel.highlightedTextColor = myColor
+            
+            let myNewColor = UIColor.init(red: 0.7, green: 0.7, blue: 0.7, alpha: 0.9)
+            self.answerLabel.highlightedTextColor = myNewColor
+        }
+        UIView.animate(withDuration: 0.5, animations: animationClosure)
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        // Set the label's initial alpha
+        questionLabel.alpha = 0
+        answerLabel.alpha = 0
+    }
+}
