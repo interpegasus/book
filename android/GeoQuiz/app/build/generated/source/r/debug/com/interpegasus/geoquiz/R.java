768d4a1764a42fb2645295ae9da1a0f12e2e88f2
Arturo G <arturo@interpegasus.com>
2016-10-19 23:47:15 -0500

    android

--- android/GeoQuiz/app/build/generated/source/r/debug/com/interpegasus/geoquiz/R.java
+++ android/GeoQuiz/app/build/generated/source/r/debug/com/interpegasus/geoquiz/R.java
@@ -1775,10 +1775,12 @@ containing a value of this type.
         public static final int abc_textfield_search_default_mtrl_alpha=0x7f020050;
         public static final int abc_textfield_search_material=0x7f020051;
         public static final int abc_vector_test=0x7f020052;
-        public static final int notification_template_icon_bg=0x7f020053;
+        public static final int arrow_left=0x7f020053;
+        public static final int arrow_right=0x7f020054;
+        public static final int notification_template_icon_bg=0x7f020055;
     }
     public static final class id {
-        public static final int action0=0x7f0b0058;
+        public static final int action0=0x7f0b0059;
         public static final int action_bar=0x7f0b0045;
         public static final int action_bar_activity_content=0x7f0b0000;
         public static final int action_bar_container=0x7f0b0044;
@@ -1787,7 +1789,7 @@ containing a value of this type.
         public static final int action_bar_subtitle=0x7f0b0025;
         public static final int action_bar_title=0x7f0b0024;
         public static final int action_context_bar=0x7f0b0046;
-        public static final int action_divider=0x7f0b005c;
+        public static final int action_divider=0x7f0b005d;
         public static final int action_menu_divider=0x7f0b0002;
         public static final int action_menu_presenter=0x7f0b0003;
         public static final int action_mode_bar=0x7f0b0042;
@@ -1800,9 +1802,9 @@ containing a value of this type.
         public static final int beginning=0x7f0b001a;
         public static final int bottom=0x7f0b0022;
         public static final int buttonPanel=0x7f0b002e;
-        public static final int cancel_action=0x7f0b0059;
+        public static final int cancel_action=0x7f0b005a;
         public static final int checkbox=0x7f0b003c;
-        public static final int chronometer=0x7f0b005f;
+        public static final int chronometer=0x7f0b0060;
         public static final int collapseActionView=0x7f0b001e;
         public static final int contentPanel=0x7f0b0034;
         public static final int custom=0x7f0b003a;
@@ -1812,7 +1814,7 @@ containing a value of this type.
         public static final int disableHome=0x7f0b000c;
         public static final int edit_query=0x7f0b0047;
         public static final int end=0x7f0b001b;
-        public static final int end_padder=0x7f0b0064;
+        public static final int end_padder=0x7f0b0065;
         public static final int expand_activities_button=0x7f0b0028;
         public static final int expanded_menu=0x7f0b003b;
         public static final int false_button=0x7f0b0056;
@@ -1821,19 +1823,20 @@ containing a value of this type.
         public static final int icon=0x7f0b002c;
         public static final int ifRoom=0x7f0b001f;
         public static final int image=0x7f0b0029;
-        public static final int info=0x7f0b0063;
-        public static final int line1=0x7f0b005d;
-        public static final int line3=0x7f0b0061;
+        public static final int info=0x7f0b0064;
+        public static final int line1=0x7f0b005e;
+        public static final int line3=0x7f0b0062;
         public static final int listMode=0x7f0b0009;
         public static final int list_item=0x7f0b002b;
-        public static final int media_actions=0x7f0b005b;
+        public static final int media_actions=0x7f0b005c;
         public static final int middle=0x7f0b001c;
         public static final int multiply=0x7f0b0014;
         public static final int never=0x7f0b0020;
-        public static final int next_button=0x7f0b0057;
+        public static final int next_button=0x7f0b0058;
         public static final int none=0x7f0b000e;
         public static final int normal=0x7f0b000a;
         public static final int parentPanel=0x7f0b0030;
+        public static final int prev_button=0x7f0b0057;
         public static final int progress_circular=0x7f0b0005;
         public static final int progress_horizontal=0x7f0b0006;
         public static final int question_text_view=0x7f0b0054;
@@ -1862,14 +1865,14 @@ containing a value of this type.
         public static final int src_atop=0x7f0b0016;
         public static final int src_in=0x7f0b0017;
         public static final int src_over=0x7f0b0018;
-        public static final int status_bar_latest_event_content=0x7f0b005a;
+        public static final int status_bar_latest_event_content=0x7f0b005b;
         public static final int submenuarrow=0x7f0b003f;
         public static final int submit_area=0x7f0b0050;
         public static final int tabMode=0x7f0b000b;
-        public static final int text=0x7f0b0062;
-        public static final int text2=0x7f0b0060;
+        public static final int text=0x7f0b0063;
+        public static final int text2=0x7f0b0061;
         public static final int textSpacerNoButtons=0x7f0b0037;
-        public static final int time=0x7f0b005e;
+        public static final int time=0x7f0b005f;
         public static final int title=0x7f0b002d;
         public static final int title_template=0x7f0b0032;
         public static final int top=0x7f0b0023;
@@ -1914,18 +1917,19 @@ containing a value of this type.
         public static final int abc_search_view=0x7f040018;
         public static final int abc_select_dialog_material=0x7f040019;
         public static final int activity_quiz=0x7f04001a;
-        public static final int notification_media_action=0x7f04001b;
-        public static final int notification_media_cancel_action=0x7f04001c;
-        public static final int notification_template_big_media=0x7f04001d;
-        public static final int notification_template_big_media_narrow=0x7f04001e;
-        public static final int notification_template_lines=0x7f04001f;
-        public static final int notification_template_media=0x7f040020;
-        public static final int notification_template_part_chronometer=0x7f040021;
-        public static final int notification_template_part_time=0x7f040022;
-        public static final int select_dialog_item_material=0x7f040023;
-        public static final int select_dialog_multichoice_material=0x7f040024;
-        public static final int select_dialog_singlechoice_material=0x7f040025;
-        public static final int support_simple_spinner_dropdown_item=0x7f040026;
+        public static final int layout=0x7f04001b;
+        public static final int notification_media_action=0x7f04001c;
+        public static final int notification_media_cancel_action=0x7f04001d;
+        public static final int notification_template_big_media=0x7f04001e;
+        public static final int notification_template_big_media_narrow=0x7f04001f;
+        public static final int notification_template_lines=0x7f040020;
+        public static final int notification_template_media=0x7f040021;
+        public static final int notification_template_part_chronometer=0x7f040022;
+        public static final int notification_template_part_time=0x7f040023;
+        public static final int select_dialog_item_material=0x7f040024;
+        public static final int select_dialog_multichoice_material=0x7f040025;
+        public static final int select_dialog_singlechoice_material=0x7f040026;
+        public static final int support_simple_spinner_dropdown_item=0x7f040027;
     }
     public static final class mipmap {
         public static final int ic_launcher=0x7f030000;
@@ -1969,15 +1973,16 @@ containing a value of this type.
         public static final int incorrect_toast=0x7f060025;
         public static final int label_text=0x7f060026;
         public static final int next_button=0x7f060027;
-        public static final int question_africa=0x7f060028;
-        public static final int question_americas=0x7f060029;
-        public static final int question_asia=0x7f06002a;
-        public static final int question_mideast=0x7f06002b;
-        public static final int question_oceans=0x7f06002c;
-        public static final int question_text=0x7f06002d;
+        public static final int prev_button=0x7f060028;
+        public static final int question_africa=0x7f060029;
+        public static final int question_americas=0x7f06002a;
+        public static final int question_asia=0x7f06002b;
+        public static final int question_mideast=0x7f06002c;
+        public static final int question_oceans=0x7f06002d;
+        public static final int question_text=0x7f06002e;
         public static final int search_menu_title=0x7f060013;
         public static final int status_bar_notification_info_overflow=0x7f060014;
-        public static final int true_button=0x7f06002e;
+        public static final int true_button=0x7f06002f;
     }
     public static final class style {
         public static final int AlertDialog_AppCompat=0x7f08008a;
--- android/GeoQuiz/app/src/main/java/com/interpegasus/geoquiz/QuizActivity.java
+++ android/GeoQuiz/app/src/main/java/com/interpegasus/geoquiz/QuizActivity.java
@@ -6,12 +6,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.util.Log;
 
 public class QuizActivity extends AppCompatActivity {
+    private static final String TAG = "QuizActivity";
+    private static final String KEY_INDEX = "index";
 
     private Button mTrueButton;
     private Button mFalseButton;
     private Button mNextButton;
+    private Button mPrevButton;
     private TextView mQuestionTextView;
 
     private Question[] mQuestionBank = new Question[] {
@@ -68,7 +72,7 @@ public class QuizActivity extends AppCompatActivity {
             }
         });
 
-        
+
         mNextButton = (Button) findViewById(R.id.next_button);
         mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -77,6 +81,60 @@ public class QuizActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mCurrentIndex == 0){
+                    mCurrentIndex = mQuestionBank.length;
+                }
+                mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                updateQuestion();
+            }
+        });
+
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
+
         updateQuestion();
     }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() called");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() called");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() called");
+    }
+
+
 }
