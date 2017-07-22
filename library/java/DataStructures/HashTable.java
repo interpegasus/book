71f7e7fb28925c0344a56c59775e5acf7da811a0
arturo <arturo@interpegasus.com>
2017-07-22 14:20:27 -0500

    Hash and List implementation

---
+++ library/java/DataStructures/HashTable.java
@@ -0,0 +1,23 @@
+import LinkedList.*;
+
+public class HashTable<E> {
+
+	private int[] hashTableArray;
+
+	// constructor
+	public HashTable() {
+		hashTableArray=null;
+	}
+
+	// Map key to index in array
+	private int hashTableIndex() {
+		return  1 ;//Arrays.hashCode(hashTableArray);
+	}
+	// Store key value in index in array in linked list
+	
+	// create a function that computes the index from a key
+
+	public static void main(String[] args){
+
+	}
+}
