530728c1588d181ba798e2aad8c835a64e37ff1b
Arturo G <arturo@interpegasus.com>
2017-08-25 12:08:27 -0500

    Stack

--- library/java/c3/stack.java
+++ library/java/c3/stack.java
@@ -1,10 +1,46 @@
-public class Stack {	
+public class Stack <T> {
+	private class StackNode <T> {
+		private T data;
+		private StackNode<T> next;
+
+		public StackNode(T element) {
+			this.data = element;
+			this.next = null;			
+		}
+	}
+
+	private StackNode<T> top;
 
 	public Stack() {
+		this.top = null;
+	}	
 
+	public void push(T data){
+		StackNode<T> myNode = new StackNode<T>(data);
+		myNode.next = this.top;
+		this.top = myNode;
 	}
 
-	public static void main(String[] args) {
-
+	public T peek(){
+		return this.top.data;
 	}
+
+	public T pop(){		
+		T data = null;
+		if (this.top != null){
+			data = this.top.data;
+			this.top = this.top.next;
+		}				
+		return data;
+	}
+
+	public static void main(String [] args){
+		Stack<Integer> myStack = new Stack<Integer>();
+		myStack.push(7);
+		myStack.push(4);
+		myStack.push(2);
+		myStack.push(1);
+		System.out.println("P");
+		System.out.println(myStack.peek());
+	}	
 }
