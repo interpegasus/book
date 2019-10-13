70897f1379b912f04fe4798d8d1982dc08a12d43
arturo <arturo@Arturos-MacBook.local>
2019-10-12 20:49:58 -0500

    Data Structures

--- iOS/WT2/WT2/ConversionViewController.swift
+++ iOS/WT2/WT2/ConversionViewController.swift
@@ -1,91 +1,74 @@
 //
-//  ViewController.swift
+//  ConversionViewController.swift
+//  WT2
 //
-//  Created by Arturo Gomez on 10/17/16.
-//  Copyright © 2016 Arturo Gomez. All rights reserved.
+//  Created by Arturo Gomez on 4/6/18.
+//  Copyright © 2018 Arturo Gomez. All rights reserved.
 //
 
 import UIKit
 
-class ConversionViewController: UIViewController, UITextFieldDelegate {
-    @IBOutlet var celsiusLabel:UILabel!
-    @IBOutlet var textField:UITextField!
+class ConversionViewController: UIViewController {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var farTextField: UITextField!
     
-    // Delegate Implementation on UITextField
-    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
-        
-        let existingTextHasDecimalSeparator = textField.text?.range(of: ".")
-        let replacementTextHasDecimalSeparator = string.range(of: ".")
-        
-        if existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil {
-            return false
-        }
-        else {
-            return true
-        }
-    }
-    
-    @IBAction func FareheithTextChanged(textField: UITextField) {
-        if let text = textField.text, let number = numberFortmatter.number(from: text) {
-            farenheithValue = number.doubleValue
-        }
-        else {
-            farenheithValue = nil
-        }
-    }
-    
-    
-    @IBAction func dismissKeyboard(sender: AnyObject){
-        textField.resignFirstResponder()
-    }
-    
-    var farenheithValue: Double? {
+    var fahrenheitValue: Measurement<UnitTemperature>? {
         didSet {
             updateCelsiusLabel()
         }
     }
     
-    var celsiusValue: Double? {
-        if let value = farenheithValue {
-            return (value - 32) * 5/9
-        }
-        else {
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
             return nil
         }
     }
     
-    let numberFortmatter: NumberFormatter = {
+    let numberFormatter: NumberFormatter = {
         let nf = NumberFormatter()
-        nf.numberStyle = .decimal
         nf.minimumFractionDigits = 0
         nf.maximumFractionDigits = 1
-        nf.alwaysShowsDecimalSeparator = true
         return nf
     }()
     
     func updateCelsiusLabel() {
-        if let value = celsiusValue {
-            celsiusLabel.text = numberFortmatter.string(for: value)
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
         } else {
             celsiusLabel.text = "???"
         }
     }
     
     override func viewDidLoad() {
-        
         super.viewDidLoad()
-        let firstFrame = CGRect(x: 100, y: 500, width: 100, height: 100);
-        let frame = UIView(frame: firstFrame);
-        frame.backgroundColor = (UIColor.magenta);
-        view.addSubview(frame);
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updateCelsiusLabel()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func dismissKeyboard(_ sender: AnyObject) {
+        farTextField.resignFirstResponder()
     }
     
+    func textField(_ farTextField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        
+        let existingTextHasDecimalSeparator = farTextField.text?.range(of: ".")
+        let replacementTextHasDecimalSeparator = string.range(of: ".")
+        
+        if existingTextHasDecimalSeparator != nil && replacementTextHasDecimalSeparator != nil {
+            return false
+        } else {
+            return true
+        }
+    }
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ farTextField: UITextField) {
+        if let text = farTextField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
+    }
     
 }
-
