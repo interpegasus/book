122214daaae66d885e13b75840fd9754aaec3063
arturo <arturo@Arturos-MacBook.local>
2019-10-19 17:46:30 -0500

    stack

--- python/data_structures/stack.py
+++ python/data_structures/stack.py
@@ -1,31 +1,84 @@
-class Node:
-	def __init__(self, data, next):
-		self.data = data
-		self.next = next
+class Node():
+    def __init__(self,data, next, prev):
+        self.data = data
+        self.next = next
 
-class Stack:
-	def __init__(self):
-		self.head = None
-	
-	def push(self, data):
-		self.head = Node(data, self.head)
+class Stack():
+    def __init__(self):
+        self.head = None        
+        self.length = 0
+    
+    def is_empty(self):
+        return self.length == 0
 
-	def pop(self):
-		data = self.head.data
-		self.head = self.head.next
-		return data 
+    def pop(self):
+        if self.is_empty():
+            return None
+        else:
+            head_value = self.head.data
+            self.head = self.head.next
+            self.length -= 1
+            return head_value
+
+    def push(self,data):
+        new_head = Node(data, None, None)        
+        if self.is_empty():
+            self.head = new_head
+        else:
+            new_head.next = self.head
+            self.head = new_head
+        self.length += 1
+        return   
+
+    def print_stack(self):
+        current_node = self.head
+        while(current_node is not None):
+            print("{}\n".format(current_node.data))
+            current_node = current_node.next
 
 
-class StackArray:
-	
-	def __init__(self):
-		self.data = []
+class TestStack:
+    def __init__(self):
+        # call tests
+        self.test_is_empty()
+        self.test_push()
+        self.test_pop()
+        self.test_print()
 
-	def push(self, item):
-		self.data.append(item)
+    def test_is_empty(self):
+        new_stack = Stack()        
+        assert new_stack.is_empty() == True
+        new_stack.push(1)
+        assert new_stack.is_empty() == False
+        print('Test stack is empty: succesfull.')
 
-	def pop(self):
-		return self.data.pop()
+    def test_push(self):
+        new_stack = Stack() 
+        items = [1,2,3,4,5,6,7]
+        for data in items:
+          new_stack.push(data)
+        assert len(items) == new_stack.length
+        assert  new_stack.length == 7        
+        print('Test insert in stack: succesfull.')
 
-	def peek(self):
-		return self.data[-1]        
+    def test_pop(self):        
+        new_stack = Stack() 
+        items = [1,2,3,4,5,6,7]
+        for data in items:
+          new_stack.push(data)  
+        removed = new_stack.pop()
+        assert removed == 7
+        removed = new_stack.pop()
+        assert removed == 6
+        assert new_stack.length == 5
+        print('Test remove in stack: succesfull.')
+
+    def test_print(self):
+        new_stack = Stack() 
+        items = [1,2,3,4,5,6,7]
+        for data in items:
+          new_stack.push(data)  
+        new_stack.print_stack()    
+        print('Test print stack: succesfull.')
+
+TestStack()
